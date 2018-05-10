4b18d0b7696a0f238d178f9c791254610eac9917
CristianoLeite <cristiano.leite@ufpr.br>
2018-05-10 17:49:55 -0300

    endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/cristiano/cursomc/domain/Cliente.java
+++ src/main/java/com/cristiano/cursomc/domain/Cliente.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import com.cristiano.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.CollectionTable;
@@ -38,6 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
