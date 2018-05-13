40337724126b463899e43e068b09a3c7e5370dad
CristianoLeite <cristiano.leite@ufpr.br>
2018-05-12 21:25:18 -0300

    validação sintatica com bean validation

--- src/main/java/com/cristiano/cursomc/services/CategoriaService.java
+++ src/main/java/com/cristiano/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cristiano.cursomc.domain.Categoria;
+import com.cristiano.cursomc.dto.CategoriaDTO;
 import com.cristiano.cursomc.repositories.CategoriaRepository;
 import com.cristiano.cursomc.services.exceptions.DataIntegrityException;
 import com.cristiano.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,5 +58,9 @@ public class CategoriaService {
 		
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 }
